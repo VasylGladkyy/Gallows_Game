498f378320ffbb57359abef5fbc984febd08a453
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-09-14 17:06:17 +0300

    add methods get_letters and cls to Methods.rb

--- Methods.rb
+++ Methods.rb
@@ -1,1 +1,16 @@
-puts "Hello world"
+def get_letters
+  if ARGV[0]==nil || ARGV[0]==""
+    print "Загадайте слово\n->"
+    return STDIN.gets.encode("UTF-8").chomp.split ""
+  else
+    return ARGV[0].encode("UTF-8").split ""
+  end
+end
+
+def cls
+  system("clear")||system("cls")
+end
+
+
+
+
