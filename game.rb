fb1d597eba6cc92b0a5c28adf8ed0818272c5d69
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-09-15 10:07:57 +0300

    add Class for class Game initialize constructor and method get_letters

--- game.rb
+++ game.rb
@@ -1,2 +1,20 @@
 class Game
+  def initialize(slovo)
+    @letters=get_letters(slovo) #to do
+    @errors=0
+    @good_letters=[]
+    @bad_letters=[]
+    @status=0
+  end
+
+  def get_letters(slovo)
+    if(slovo==nil || slovo == "")
+      abort "Ви ве ввели слово для гри!!!"
+    end
+    return slovo.split("")
+  end
+
+  def ask_next_letter
+
+  end
 end
