e2e80c2ab4c574a48165409a4aaf774d3935ecca
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-09-16 22:51:31 +0300

    add methods to works with image files

--- image_reader.rb
+++ image_reader.rb
@@ -1,2 +1,22 @@
 class ImageReader
+  def initialize(path)
+    @path=path
+  end
+
+  def getImagesArray
+    imagesArray=[]
+    for i in 0..7
+      file=@path+"#{i}.txt"
+      imagesArray<<getImage(file)
+    end
+    return imagesArray
+  end
+
+  def getImage(file)
+    if File.exist? file
+      f=File.new file,"r:UTF-8"
+      return f.readlines
+    end
+    return nil
+  end
 end
