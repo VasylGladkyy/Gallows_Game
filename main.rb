d430d461871b68ae9c2b4182dd30a03702cc1e69
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-09-16 22:57:57 +0300

    add curent path to classes, add objects to read image and word from file

--- main.rb
+++ main.rb
@@ -1,10 +1,14 @@
 #метод require дозволяє підключити файл який містить в собі певну логіку
-require './game.rb'
-require './result_console_print.rb'
+current_path=File.dirname(__FILE__ )
+require current_path+'/game.rb'
+require current_path+'/result_console_print.rb'
+require current_path+'/word_reader.rb'
+require current_path+'/image_reader.rb'
 
-printer=ResultConsolePrint.new
-slovo = ARGV[0]
-printer.cls
+image_reader=ImageReader.new current_path+"/data/image/"
+printer=ResultConsolePrint.new image_reader.getImagesArray
+reader=WordReader.new current_path+"/data/words.txt"
+slovo=reader.read_from_file
 game = Game.new(slovo)
 
 while game.status ==0 do
